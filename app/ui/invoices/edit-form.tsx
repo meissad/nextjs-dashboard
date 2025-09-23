012caf0c7a27e9d943385b8331ca7b821707eb67
Cheikh Meissa Diagne <sameiidia1@gmail.com>
2025-09-23 18:09:52 +0000

    error handling

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -18,8 +18,9 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const updateInvoiceWithId =  updateInvoice.bind(null, invoice.id);
   return (
+    // @ts-expect-error Server Actions are handled by Next.js
     <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
